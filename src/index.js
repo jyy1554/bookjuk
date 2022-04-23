04e7af3f99c5d5eb3a5c7d5ea1d8cab73efa2107
jyy1554 <jyy1556@gmail.com>
2022-04-23 16:02:34 +0900

    브라우저 CORS 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+//개발 환경에선 프록시 서버 쪽으로 요청하고, 프로덕션에선 실제 API 서버로 요청
+//API 도메인이 실서비스의 도메인과 달라 추가해줌
+axios.defaults.baseURL =
+    process.env.NODE_ENV === "development" ? "/" : "https://www.aladin.co.kr";
